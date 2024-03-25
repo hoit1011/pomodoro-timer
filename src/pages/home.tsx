dce17e52f76ee56f140d02998b77259c726d0c1e
hoit1011 <parksm071011@gmail.com>
2024-03-25 15:18:17 +0900

    front-end

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,21 +5,20 @@ const ClockHand: React.FC = () => {
   const [isDragging, setIsDragging] = useState<boolean>(false)
   const [minute, setMinute] = useState<number>(0)
   const clockRef = useRef<HTMLDivElement>(null)
+  const [isStarted, setIsStarted] = useState<boolean>(false)
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      if (isDragging) {
+      if (isDragging && !isStarted) { 
         const centerX = window.innerWidth / 2
         const centerY = window.innerHeight / 2
         const newAngle = ((Math.atan2(event.clientY - centerY, event.clientX - centerX) * (180 / Math.PI)) + 90) % 360
-        const miniteAngle = Math.round(newAngle / 6)
-        setMinute(miniteAngle < 0 ? miniteAngle + 61 : miniteAngle)
+        const minuteAngle = Math.ceil(newAngle / 6)
+        setMinute(minuteAngle <= 0 ? minuteAngle + 60 : minuteAngle)
         setAngle(newAngle < 0 ? newAngle + 360 : newAngle)
       }
-    };
+    }
 
-
-    
     const startDragging = () => setIsDragging(true)
     const stopDragging = () => setIsDragging(false)
 
@@ -31,20 +30,49 @@ const ClockHand: React.FC = () => {
       window.removeEventListener('mousemove', handleMouseMove)
       window.removeEventListener('mousedown', startDragging)
       window.removeEventListener('mouseup', stopDragging)
+    }
+  }, [isDragging, isStarted])
+
+  useEffect(() => {
+    let interval: number
+
+    if (isStarted) {
+      interval = setInterval(() => {
+        setAngle((prevAngle) => prevAngle - 0.01)
+      }, 100)
+      interval = setInterval(() => {
+        setMinute((prevMinute) => prevMinute - 1)
+      },60000)
+    }
+
+    return () => {
+      clearInterval(interval)
     };
-  }, [isDragging])
+  }, [isStarted])
+
+  const handleStartBtnClick = () => {
+    setIsStarted(true)
+  };
+
   return (
     <div className="clock" ref={clockRef}>
       <div
         className="stick"
-        style={{ 
+        style={{
           transform: `rotate(${angle}deg)`,
         }}
       ></div>
-      <div className="fill" style={{ 
-          background: `conic-gradient(red ${angle}deg, transparent 0deg)`,
-        }}></div>
-      <div className='time'>{minute}</div>
+      <div
+        className="fill"
+        style={{
+          background: `conic-gradient(${isStarted ? 'blue' : 'red'} ${angle}deg, transparent 0deg)`,
+        }}
+      ></div>
+      <button className="startBtn" onClick={handleStartBtnClick}>
+        시작!
+      </button>
+      <div className="time">{minute}</div>
+      <div className='time' style={{ left:500}}> {angle}</div>
     </div>
   );
 };
