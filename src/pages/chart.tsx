80904e65a1772ce07b439aa139ef2853f29ecce6
hoit1011 <parksm071011@gmail.com>
2024-04-17 00:12:17 +0900

    chart값 받기

--- src/pages/chart.tsx
+++ src/pages/chart.tsx
@@ -1,7 +1,29 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const Chart = () => {
+    const token = localStorage.getItem('token')
+    const subject: any[] = []
+    const time: any[] = []
+    const fetchdata = async() => {
+        const response = await axios.get('http://localhost:8080/getchart', {
+            headers: {
+                Authorization: token
+            }
+        })
+        for(let i = 0; i < response.data.data.length; i++){
+            subject.push(response.data.data[i].subject)
+            time.push(response.data.data[i]._sum.study_time)
+        }
+        console.log(subject)
+        console.log(time)
+    }
+    useEffect(() => {
+        fetchdata()
+    },[])
+
     return (
-        <div>ㅎㅇ</div>
+        <div>asd</div>
     )
 }
 
