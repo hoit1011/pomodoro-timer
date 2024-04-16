5577e34e73815332fc5345a82f3a305cf6bfa1ad
hoit1011 <parksm071011@gmail.com>
2024-04-16 20:14:22 +0900

    study db 저장

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,26 +11,27 @@ const ClockHand: React.FC = () => {
   const clockRef = useRef(null)
   const [isStarted, setIsStarted] = useState(false)
   const token = localStorage.getItem('token')
+  const [what, setWhat] = useState("") 
 
   const chartpage = async () => {
     const response = await axios.get('http://localhost:8080/chart', {
       headers: {
         Authorization: token
       }
-    })  
+    })
     console.log(response)
-    if(response.data.status === 200){
+    if (response.data.status === 200) {
       window.location.href = '/chart'
-    }else{
+    } else {
       alert('로그인이 필요합니다.')
       window.location.href = '/Signup'
       localStorage.removeItem('token')
     }
   }
- 
+
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      if (isDragging && !isStarted) { 
+      if (isDragging && !isStarted) {
         const centerX = window.innerWidth / 2
         const centerY = window.innerHeight / 2
         const newAngle = ((Math.atan2(event.clientY - centerY, event.clientX - centerX) * (180 / Math.PI)) + 90) % 360
@@ -55,47 +56,73 @@ const ClockHand: React.FC = () => {
   }, [isDragging, isStarted])
 
   useEffect(() => {
-    let angleInterval: number;
-    let minuteInterval: number;
-  
+    let angleInterval: number
+    let minuteInterval: number
+    const result: number = minute
+    const fetchdata = async () => {
+      console.log(what, result)
+      const response = await axios.post('http://localhost:8080/study',{
+        what,
+        result
+      },{
+      headers: {
+        Authorization: token
+      }
+    })
+      console.log(response)
+      if(response.data.status === 200){
+        alert('기록이 저장되었습니다.')
+      }else{
+        alert('에러로 기록이 날라갔습니다')
+      }
+    }
     if (isStarted) {
       angleInterval = setInterval(() => {
         setAngle((prevAngle) => {
           const newAngle = prevAngle - 0.01;
           if (newAngle <= 0) {
-            clearInterval(angleInterval); 
-            setIsStarted(false);
-            setMinute(0);
+            clearInterval(angleInterval)
+            setIsStarted(false)
+            setMinute(0)
+            fetchdata()
             return 0;
           }
           return newAngle;
         });
       }, 100);
-  
+
       minuteInterval = setInterval(() => {
         setMinute((prevMinute) => prevMinute - 1);
       }, 60000);
     }
-  
+
     return () => {
       clearInterval(angleInterval);
       clearInterval(minuteInterval);
     };
   }, [isStarted]);
-  
 
   const handleStartBtnClick = () => {
+    const input = prompt('무엇을 할 시간인가요 ?')
+    if (input === "") {
+      alert('입력을 하셔야합니다.')
+      return
+    }
+    if (!input) {
+      throw new Error('No saved');
+    }
+    setWhat(input)
     setIsStarted(true)
   };
 
   return (
     <div>
-      {token &&(<div className='chart' onClick={chartpage}>
-          <FaChartPie/><span style={{padding:20, fontWeight:'bold'}}>차트보러가기</span>
+      {token && (<div className='chart' onClick={chartpage}>
+        <FaChartPie /><span style={{ padding: 20, fontWeight: 'bold' }}>차트보러가기</span>
       </div>)}
-      {!token &&(<div className='Signup'>혹시 아직 회원가입을 안하셨다면 ..?
+      {!token && (<div className='Signup'>혹시 아직 회원가입을 안하셨다면 ..?
         <span className='Signupbtn'>
-          <Link to="Signup" style={{textDecoration: 'none', color: 'gray'}}>
+          <Link to="Signup" style={{ textDecoration: 'none', color: 'gray' }}>
             회원가입
           </Link>
         </span>
